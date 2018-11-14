c335b049b0020ed7493d52d37373f341b43e6b1d
lllsf <188101255214@163.com>
2018-11-14 11:44:17 +0800

    fisrt commit

--- org/urls.py
+++ org/urls.py
@@ -9,8 +9,14 @@
 
 from django.urls import path
 from .views import home
-
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('<int:org_id>/', home),
-]
+    path('register01/',views.register01),
+    path('register02/',views.register02),
+    path('myhome/',views.myhome),
+    path('home01/',views.home01),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- org/views.py
+++ org/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 
 
 @login_required(login_url='/user/login')
 def home(request, org_id):
-    return HttpResponse('hello'+str(org_id))
+    return HttpResponse('hello' + str(org_id))
+
+
+def register01(request):
+    if request.method == 'POST':
+        new_img = IMG(img=request.FILES.get('img'))
+        new_img.save()
+    return render(request, 'register01.html')
+
+
+def register02(request):
+    return render(request, 'register02.html')
+
+
+def myhome(request):
+    return render(request, 'myhome.html')
+
+
+def home01(request):
+    return render(request, 'home.html')
