f28c0ad71d4325c51322549a4c90a2733447974c
Ada <ada_0504@163.com>
2018-11-16 15:04:14 +0800

    update login.html,regiter.html and index.html

--- recruit/settings.py
+++ recruit/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'USER': 'root',
-        'PASSWORD': '1234.Com',
-        'HOST': '192.168.137.20',
+        'PASSWORD': 'yaoye',
+        'HOST': 'localhost',
         'PORT': '3306',
-        'NAME': 'recruit',
+        'NAME': 'recruit2',
 
     }
 }
